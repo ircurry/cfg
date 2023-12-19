6136b70791a08f6a4bb9987514934bb555279856
Ian Curran <icurran@protonmail.com>
2023-12-19 18:53:46 -0500

    feat: add neofetch to core cli packages

--- modules/home/cli/core.nix
+++ modules/home/cli/core.nix
@@ -3,9 +3,10 @@
 {
   
   home.packages = with pkgs; [
-    file # file info
-    fzf  # cli fuzzy finder
-    vim  # cli text editor (I don't want to use nano)
+    file     # file info
+    fzf      # cli fuzzy finder
+    neofetch # system info fetcher
+    vim      # cli text editor (I don't want to use nano)
   ];
 
 }
