4a0c9f16d0878235c7256a056f6649dab9b6e76f
Ian Curran <icurran@protonmail.com>
2023-12-24 01:37:13 -0500

    feat: turn caps-lock into control

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -125,8 +125,8 @@
         
         # Example per-device config
         # See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
-        device:epic-mouse-v1 {
-            sensitivity = -0.5
+        device:at-translated-set-2-keyboard {
+            kb_options = ctrl:nocaps
         }
         
         # Example windowrule v1
