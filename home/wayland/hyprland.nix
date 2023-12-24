213a116cd165af627542fb01596a3ce7ba6d9b2a
Ian Curran <icurran@protonmail.com>
2023-12-24 01:36:57 -0500

    fix: focus doesn't follow mouse

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -45,7 +45,7 @@
             kb_options =
             kb_rules =
         
-            follow_mouse = 1
+            follow_mouse = 2
         
             touchpad {
                 natural_scroll = no
