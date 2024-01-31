5e0a8c1c88d534d4e1f8f1c4075360049a670a43
Ian Curran <icurran@protonmail.com>
2024-01-31 11:13:13 -0500

    feat(alacritty): colors declared via themes

--- home/options/graphical.nix
+++ home/options/graphical.nix
@@ -1,9 +1,83 @@
-{ lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   options.nocturne.graphical = {
-    alacritty.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Enable Alacritty";
+    alacritty = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable Alacritty";
+      };
+      bg = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base00;
+      };
+      fg = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base05;
+      };
+      black = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base01;
+      };
+      red = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base08;
+      };
+      yellow = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0A;
+      };
+      green = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0B;
+      };
+      cyan = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0C;
+      };
+      blue = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0D;
+      };
+      magenta = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0E;
+      };
+      white = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base04;
+      };
+      bright-black = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base03;
+      };
+      bright-red = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base08;
+      };
+      bright-yellow = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0A;
+      };
+      bright-green = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0B;
+      };
+      bright-cyan = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0C;
+      };
+      bright-blue = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0D;
+      };
+      bright-magenta = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base0E;
+      };
+      bright-white = lib.mkOption {
+        type = lib.types.str;
+        default = config.nocturne.themes.colors.base06;
+      };
     };
     brave.enable = lib.mkEnableOption "Enable Brave Browser";
     emacs = {
--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -3,6 +3,24 @@
 let
   cfg = config.nocturne.graphical.alacritty;
   way-cfg = config.nocturne.wayland.terminal;
+  bg = config.nocturne.graphical.alacritty.bg; # ----
+  fg = config.nocturne.graphical.alacritty.fg; # ++
+  black = config.nocturne.graphical.alacritty.black; # ---
+  red = config.nocturne.graphical.alacritty.red; # red
+  yellow = config.nocturne.graphical.alacritty.yellow; # yellow
+  green = config.nocturne.graphical.alacritty.green; # green
+  cyan = config.nocturne.graphical.alacritty.cyan; # aqua/cyan
+  blue = config.nocturne.graphical.alacritty.blue; # blue
+  magenta = config.nocturne.graphical.alacritty.magenta; # purple
+  white = config.nocturne.graphical.alacritty.white; # +
+  bright-black = config.nocturne.graphical.alacritty.bright-black; # -
+  bright-red = config.nocturne.graphical.alacritty.bright-red; # red
+  bright-yellow = config.nocturne.graphical.alacritty.bright-yellow; # yellow
+  bright-green = config.nocturne.graphical.alacritty.bright-green; # green
+  bright-cyan = config.nocturne.graphical.alacritty.bright-cyan; # ~, normally aqua/cyan
+  bright-blue = config.nocturne.graphical.alacritty.bright-blue; # blue
+  bright-magenta = config.nocturne.graphical.alacritty.bright-magenta; # purple
+  bright-white = config.nocturne.graphical.alacritty.bright-white; # +++
 in
 {
   config = lib.mkMerge [
@@ -54,32 +72,32 @@ in
           ];
           colors = {
             primary = {
-              background = "0x2e3440";
-              foreground = "0xd8dee9";
+              background = "0x${bg}";
+              foreground = "0x${fg}";
             };
             cursor = {
-              cursor = "0xd8dee9";
-              text = "0x2e3440";
+              cursor = "0x${fg}";
+              text = "0x${bg}";
             };
             normal = {
-              black = "0x3b4252";
-              red = "0xbf616a";
-              green = "0xa3be8c";
-              yellow = "0xebcb8b";
-              blue = "0x81a1c1";
-              magenta = "0xb48ead";
-              cyan = "0x88c0d0";
-              white = "0xe5e9f0";
+              black = "0x${black}";
+              red = "0x${red}";
+              green = "0x${green}";
+              yellow = "0x${yellow}";
+              blue = "0x${blue}";
+              magenta = "0x${magenta}";
+              cyan = "0x${cyan}";
+              white = "0x${white}";
             };
             bright = {
-              black = "0x4c566a";
-              red = "0xbf616a";
-              green = "0xa3be8c";
-              yellow = "0xebcb8b";
-              blue = "0x81a1c1";
-              magenta = "0xb48ead";
-              cyan = "0x8fbcbb";
-              white = "0xeceff4";
+              black = "0x${bright-black}";
+              red = "0x${bright-red}";
+              green = "0x${bright-green}";
+              yellow = "0x${bright-yellow}";
+              blue = "0x${bright-blue}";
+              magenta = "0x${bright-magenta}";
+              cyan = "0x${bright-cyan}";
+              white = "0x${bright-white}";
             };
           };
         };
