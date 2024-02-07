e13d085bb6cf2e0038ad7b051192bd45e0c8d15f
Ian Curran <icurran@protonmail.com>
2024-02-07 18:42:10 -0500

    feat(hyprland): removed unnecessary shadow configuration

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -65,9 +65,6 @@ in {
             passes = 2;
           };
           drop_shadow = "no";
-          shadow_range = 4;
-          shadow_render_power = 3;
-          "col.shadow" = "rgba(1a1a1aee)";
         };
         animations = {
           enabled = "yes";
