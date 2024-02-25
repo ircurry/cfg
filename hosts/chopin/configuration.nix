d3cd5100ceb37ef26ae3f655f76d475175d696e1
Ian Curran <icurran@protonmail.com>
2024-02-25 18:29:35 -0500

    refactor(nixos): networking module

--- hosts/chopin/configuration.nix
+++ hosts/chopin/configuration.nix
@@ -16,16 +16,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "chopin"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Set your time zone.
   time.timeZone = "America/New_York";
 
