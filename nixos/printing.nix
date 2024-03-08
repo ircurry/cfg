c59858d9685da5a1986dbc6298f95eb7ccf6788b
Ian Curran <icurran@protonmail.com>
2024-03-07 21:13:30 -0500

    feat(nixos): got nixos printing working partially
    Only works for hardwired connections. Networkprinting does not work
    properly yet.

--- nixos/printing.nix
+++ nixos/printing.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   config = {
     # Enable CUPS to print documents.
-    services.printing.enable = true;
+    services.printing = {
+      enable = true;
+      drivers = with pkgs; [ gutenprint hplip splix ];
+    };
   };
 }
