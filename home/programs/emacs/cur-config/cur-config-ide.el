3d0bbd8aa9adc65d16ab0696d6c6b07721a55baf
Ian Curran <icurran@protonmail.com>
2024-04-15 18:47:23 -0400

    feat(emacs): ide configs in emacs.org

--- home/programs/emacs/cur-config/cur-config-ide.el
+++ home/programs/emacs/cur-config/cur-config-ide.el
@@ -14,10 +14,10 @@
   :custom
   (lsp-keymap-prefix "C-c C-M-l" "lsp mode keymap")
   :bind (:map lsp-mode-map
-	  ("C-c C-a" . lsp-execute-code-action)  ; code actions
-	  ("C-c C-e" . lsp-treemacs-errors-list) ; treemacs error list
-	  ("C-c f"   . lsp-find-references)      ; find references
-	  ("C-c r"   . lsp-find-definition))     ; find definitions
+              ("C-c C-a" . lsp-execute-code-action)  ; code actions
+              ("C-c C-e" . lsp-treemacs-errors-list) ; treemacs error list
+              ("C-c f"   . lsp-find-references)      ; find references
+              ("C-c r"   . lsp-find-definition))     ; find definitions
   :config
   (lsp-deferred)
   (setq gc-cons-threshold (* 100 1024 1024))
@@ -34,16 +34,16 @@
   (lsp-ui-doc-show-with-mouse t "lsp-ui doc follows mouse")
   (lsp-ui-doc-position 'at-point "lsp-ui doc shows at cursor")
   :bind (:map lsp-ui-mode-map
-	 ("C-c C-f" . lsp-ui-peek-find-references)  ; find references ui
-	 ("C-c C-r" . lsp-ui-peek-find-definitions) ; find definitions ui
-	 ("C-c C-d" . lsp-ui-doc-mode) ; toggle doc mode
-	 :map lsp-ui-peek-mode-map
-	 ("ESC" . lsp-ui-peek--abort)             ; toggle doc mode
-	 ("g"   . lsp-ui-peek--abort)             ; toggle doc mode
-	 ("j"   . lsp-ui-peek--select-next)       ; toggle doc mode
-	 ("k"   . lsp-ui-peek--select-prev)       ; toggle doc mode
-	 ("C-j" . lsp-ui-peek--select-next-file)  ; toggle doc mode
-	 ("C-k" . lsp-ui-peek--select-prev-file)) ; toggle doc mode
+              ("C-c C-f" . lsp-ui-peek-find-references)  ; find references ui
+              ("C-c C-r" . lsp-ui-peek-find-definitions) ; find definitions ui
+              ("C-c C-d" . lsp-ui-doc-mode) ; toggle doc mode
+              :map lsp-ui-peek-mode-map
+              ("ESC" . lsp-ui-peek--abort)             ; toggle doc mode
+              ("g"   . lsp-ui-peek--abort)             ; toggle doc mode
+              ("j"   . lsp-ui-peek--select-next)       ; toggle doc mode
+              ("k"   . lsp-ui-peek--select-prev)       ; toggle doc mode
+              ("C-j" . lsp-ui-peek--select-next-file)  ; toggle doc mode
+              ("C-k" . lsp-ui-peek--select-prev-file)) ; toggle doc mode
   :hook
   (lsp-mode . lsp-ui-mode))
 
@@ -53,14 +53,14 @@
   (prog-mode . company-mode)
   (lsp-mode . company-mode)
   :bind (:map company-active-map
-         ("<tab>" . company-complete-common-or-cycle)
-         ("<return>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
+              ("<tab>" . company-complete-common-or-cycle)
+              ("<return>" . company-complete-selection))
+  (:map lsp-mode-map
+        ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
-  ;;(company-tng-configure-default))
+;;(company-tng-configure-default))
 
 ;; ===Flycheck===
 (use-package flycheck
@@ -72,7 +72,7 @@
 (use-package treemacs
   :bind
   (:map cur/sub-leader-keymap
-	("C-t" . treemacs-select-window))
+        ("C-t" . treemacs-select-window))
   :config
   (treemacs-follow-mode))
 
@@ -86,7 +86,7 @@
 ;; ===Magit===
 (use-package magit
   :bind (:map cur/sub-leader-keymap
-	 ("C-M-g" . magit))
+              ("C-M-g" . magit))
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
   (transient-default-level 5 "Allowing for commit signing"))
@@ -106,7 +106,7 @@
               ("C-l"   . projectile-ibuffer)
               ("C-k"   . projectile-kill-buffers)
               ("C-v"   . projectile-vc)
-	      ("C-e"   . projectile-run-eshell))
+              ("C-e"   . projectile-run-eshell))
   :config
   (projectile-mode 1))
 
