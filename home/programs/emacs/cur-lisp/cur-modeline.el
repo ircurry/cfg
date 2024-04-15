0dbbe0173cd850a906174f09aa844dfd636332e4
Ian Curran <icurran@protonmail.com>
2024-04-15 19:52:08 -0400

    feat(emacs): modeline in emacs.org

--- home/programs/emacs/cur-lisp/cur-modeline.el
+++ home/programs/emacs/cur-lisp/cur-modeline.el
@@ -18,40 +18,40 @@
   "Function to return padding so `mode-line-end-spaces' will be right aligned."
   (let ((r-length (length (format-mode-line mode-line-end-spaces))))
     (propertize " "
-		'display `(space :align-to (- right ,r-length)))))
+                'display `(space :align-to (- right ,r-length)))))
 
 (setq-default mode-line-format
-	      '("%e"
-		(:eval
-		 (when (and (mode-line-window-selected-p) defining-kbd-macro)
-		   (propertize "  KMACRO  " 'face 'font-lock-string-face)))
-		(:eval
-		 (when (and (mode-line-window-selected-p)
-			    (buffer-narrowed-p)
-			    (not (derived-mode-p 'Info-mode 'help-mode 'special-mode 'message-mode)))
-		   (propertize "  NARROWED  " 'face 'font-lock-constant-face)))
-		(:eval
-		 (when (and (member 'meow features) (mode-line-window-selected-p))
-		   (propertize (format "  %s  " (upcase (symbol-name meow--current-state)))
-			       'face 'highlight)))
-		"  "
-		(:eval
-		 (when (mode-line-window-selected-p)
-		   (cond (buffer-read-only
-			  (propertize "RO" 'face 'shadow))
-			 ((buffer-modified-p)
-			  (propertize "**" 'face 'shadow))
-			 (t
-			  (propertize "RW" 'face 'shadow)))))
-		"  "
-		(:eval
-		 (propertize (format "%s" (buffer-name)) 'face 'bold))
-		"  "
-		(:eval
-		 (if (mode-line-window-selected-p)
-		   (propertize (capitalize (symbol-name major-mode)) 'face 'warning)
-		   (propertize (capitalize (symbol-name major-mode)) 'face 'shadow)))
-		(:eval (cur-mode-line/padding))))
+              '("%e"
+                (:eval
+                 (when (and (mode-line-window-selected-p) defining-kbd-macro)
+                   (propertize "  KMACRO  " 'face 'font-lock-string-face)))
+                (:eval
+                 (when (and (mode-line-window-selected-p)
+                            (buffer-narrowed-p)
+                            (not (derived-mode-p 'Info-mode 'help-mode 'special-mode 'message-mode)))
+                   (propertize "  NARROWED  " 'face 'font-lock-constant-face)))
+                (:eval
+                 (when (and (member 'meow features) (mode-line-window-selected-p))
+                   (propertize (format "  %s  " (upcase (symbol-name meow--current-state)))
+                               'face 'highlight)))
+                "  "
+                (:eval
+                 (when (mode-line-window-selected-p)
+                   (cond (buffer-read-only
+                          (propertize "RO" 'face 'shadow))
+                         ((buffer-modified-p)
+                          (propertize "**" 'face 'shadow))
+                         (t
+                          (propertize "RW" 'face 'shadow)))))
+                "  "
+                (:eval
+                 (propertize (format "%s" (buffer-name)) 'face 'bold))
+                "  "
+                (:eval
+                 (if (mode-line-window-selected-p)
+                   (propertize (capitalize (symbol-name major-mode)) 'face 'warning)
+                   (propertize (capitalize (symbol-name major-mode)) 'face 'shadow)))
+                (:eval (cur-mode-line/padding))))
 
 (provide 'cur-modeline)
 ;;; cur-modeline.el ends here
