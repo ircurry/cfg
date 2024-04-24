c5c5539fa2d87c623df32659c380cb6f25828db1
Ian Curran <icurran@protonmail.com>
2024-04-24 15:09:32 -0400

    docs(emacs): added prose on init.el

--- home/programs/emacs/init.el
+++ home/programs/emacs/init.el
@@ -51,8 +51,8 @@
 
 ;; ===Essentials===
 (require 'cur-config-essentials)
-(require 'cur-config-bindings)
 (require 'cur-config-isearch)
+(require 'cur-config-bindings)
 (require 'cur-config-help)
 (require 'cur-config-dired)
 
