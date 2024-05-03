f10773eb11a47a4e072156752ef260c7a9899b7a
Ian Curran <icurran@protonmail.com>
2024-05-02 23:48:33 -0400

    emacs: removed eww/yuck configurations
    They are unused and just clutter the config.

--- home/programs/emacs/cur-config/cur-config-markup.el
+++ home/programs/emacs/cur-config/cur-config-markup.el
@@ -1,11 +1,5 @@
 ;; ===YAML===
 (use-package yaml-mode
-  ;:ensure t
   :commands (yaml-mode))
-
-;; ===EWW/Yuck Configuration===
-(use-package yuck-mode
-  ;:ensure t
-  :commands (yuck-mode))
 
 (provide 'cur-config-markup)
