cd42c86e93da951b4066906a0d8a07a75f9c3841
Ian Curran <icurran@protonmail.com>
2024-05-26 23:51:05 -0400

    emacs: the start of the base library

---
+++ home/programs/emacs/cur-lisp/cur-base.el
@@ -0,0 +1,14 @@
+;;; cur-base.el --- The base of my customizations for emacs
+
+;;; Commentary:
+;; This is the base of my extensions to Emac's functionality.
+;; Do try this at home kids.
+
+;;; Code:
+
+(defgroup cur-base ()
+  "Base functions and variables for my Emacs configuration."
+  :group 'editing)
+
+(provide 'cur-base)
+;;; cur-base.el ends here
