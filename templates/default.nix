b56084970f3459afa17a1f7ead35bb11ed1f0fd0
Ian Curran <icurran@protonmail.com>
2024-06-03 09:57:57 -0400

    templates: added zig flake template

--- templates/default.nix
+++ templates/default.nix
@@ -1,14 +1,18 @@
 let
-  haskell = {
-    path = ./haskell;
-    description = "Haskell flake and dev enviornment";
-  };
   c = {
     path = ./c;
     description = "C flake and dev enviornment";
   };
+  haskell = {
+    path = ./haskell;
+    description = "Haskell flake and dev enviornment";
+  };
+  zig = {
+    path = ./zig;
+    description = "Zig flake and dev enviornment";
+  };
 in
 {
-  inherit haskell c;
+  inherit c haskell zig;
   hs = haskell;
 }
