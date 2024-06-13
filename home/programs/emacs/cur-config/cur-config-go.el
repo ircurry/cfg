3aa5974db6c5134ba60505a29edf7f86b0f9d015
Ian Curran <icurran@protonmail.com>
2024-06-12 23:13:08 -0400

    emacs: fixed go.mod not being go-mod-ts-mode and tab-width

--- home/programs/emacs/cur-config/cur-config-go.el
+++ home/programs/emacs/cur-config/cur-config-go.el
@@ -1,8 +1,12 @@
 ;; ===Go Tree-Sitter Mode===
 (use-package go-ts-mode
-  :mode "\\.go\\'"
+  :mode
+  ("\\.go\\'" . go-ts-mode)
+  ("^go\\.mod\\'" . go-mod-ts-mode)
   :custom
   (go-ts-mode-indent-offset 4 "Set the indentation to 4")
-  :hook (go-ts-mode . lsp-deferred))
+  :hook
+  (go-ts-mode . lsp-deferred)
+  (go-ts-mode . (lambda () (setq tab-width 4))))
 
 (provide 'cur-config-go)
