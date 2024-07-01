f17a2e70eda8b4c53ef29a04fac901ddc251ba09
Ian Curran <icurran@protonmail.com>
2024-07-01 13:34:29 -0400

    emacs: disable helpful mode

--- home/programs/emacs/cur-config/cur-config-help.el
+++ home/programs/emacs/cur-config/cur-config-help.el
@@ -6,17 +6,6 @@
   :config
   (setq which-key-idle-delay 0.0001))
 
-;; ===helpful===
-(use-package helpful
-  ;; :custom
-  ;; (counsel-describe-function-function #'helpful-callable)
-  ;; (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ;; ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ;; ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
 ;; ===Info===
 (use-package info
   :config
