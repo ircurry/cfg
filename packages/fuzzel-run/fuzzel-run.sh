437c7ebbceb8c0f73ba5bc79566740a239e01ca7
Ian Curran <icurran@protonmail.com>
2024-07-27 20:29:43 -0400

    packages: fix fuzzel-run from weird assignment bug

--- packages/fuzzel-run/fuzzel-run.sh
+++ packages/fuzzel-run/fuzzel-run.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-
-prog=$(echo "$PATH" \
-	    | sed 's/\:/\n/g' \
-	    | xargs -I{} \
-		    find -L {} -maxdepth 1 -mindepth 1 \
-		    -type f -executable \
-		    -printf '%P\n' 2>/dev/null \
- 	    | sort -u | sed 's/^\..*//g; /^$/d' | fuzzel -d)
-
-# echo $prog
-setsid -f "$prog" >/dev/null 2>&1
+setsid -f "$(echo "$PATH" \
+		| sed 's/\:/\n/g' \
+		| xargs -I{} \
+			find -L {} -maxdepth 1 -mindepth 1 \
+			-type f -executable \
+			-printf '%P\n' 2>/dev/null \
+		| sort -u | sed 's/^\..*//g; /^$/d' | fuzzel -d)" >/dev/null 2>&1
