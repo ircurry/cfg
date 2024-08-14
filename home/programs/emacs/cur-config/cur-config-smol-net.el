0f9ace752cf32c091ac1d9139c30461ea07d36f0
Ian Curran <icurran@protonmail.com>
2024-08-13 20:20:50 -0400

    emacs: defer smol net packages

--- home/programs/emacs/cur-config/cur-config-smol-net.el
+++ home/programs/emacs/cur-config/cur-config-smol-net.el
@@ -1,11 +1,14 @@
 ;; ===Gemini-Mode===
-(use-package gemini-mode)
+(use-package gemini-mode
+  :defer t)
 
 ;; ===Org Gemini Exporter==
-(use-package ox-gemini)
+(use-package ox-gemini
+  :defer t)
 
 ;; ===Elpher Gemini/Gopher Client===
 (use-package elpher
+  :defer t
   :custom
   (elpher-default-url-type "gemini"))
 
