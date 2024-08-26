529d26746e4818f91cdabfeaeec2e908c75aee7f
Ian Curran <icurran@protonmail.com>
2024-08-26 16:57:10 -0400

    Revert "themes: switch chopin to gruvbox"
    This reverts commit 9707ca9012925cd0d7b0b12437dccb31917e8868.

--- hosts/chopin/home.nix
+++ hosts/chopin/home.nix
@@ -10,13 +10,13 @@
     # themes.theme = "catppuccin-mocha";
     # themes.theme = "grimshaw";
     # themes.theme = "gruvbox-dark";
-    themes.theme = "gruvbox-dark-bright";
+    # themes.theme = "gruvbox-dark-bright";
     # themes.theme = "gruvbox-light";
     # themes.theme = "kanagawa-wave";
     # themes.theme = "nord-aurora";
     # themes.theme = "nord-light";
     # themes.theme = "oceanic-next";
-    # themes.theme = "tomorrow-night";
+    themes.theme = "tomorrow-night";
     graphical = {
       alacritty.enable = true;
       anki.enable = true;
