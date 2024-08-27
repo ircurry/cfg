a111916b7ed572d36c568ece0c08eec3afc1bf98
Ian Curran <icurran@protonmail.com>
2024-08-27 10:41:28 -0400

    nixos: enable dev docs

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,6 +1,10 @@
 { ... }:
 {
   config = {
+    documentation = {
+      dev.enable = true;
+      info.enable = true;
+    };
     nix.settings = {
       substituters = [ "https://hyprland.cachix.org" ];
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
