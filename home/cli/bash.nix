dc380255e93841bfb36d34067f247bd9660baca8
Ian Curran <icurran@protonmail.com>
2024-08-29 15:04:39 -0400

    cli: remove redundant direnv hook and give real promt in bash

--- home/cli/bash.nix
+++ home/cli/bash.nix
@@ -10,7 +10,7 @@
     programs.bash = {
       enable = true;
       initExtra = ''
-        eval "$(direnv hook bash)"
+        PS1="\e[1;31m[\e[1;33m\u\e[1;32m@\e[1;34m\h\e[m \e[1;35m\w\e[1;31m]\e[m\e[1;37m$\e[m "
       '';
       profileExtra = config.nocturne.wayland.compositor.profileExtra;
     };
