f8d0d9c484ba0a1cedd6dcf1aacb8c442ebad714
Ian Curran <icurran@protonmail.com>
2024-10-24 07:44:58 -0400

    nixos: clean up nix.nix

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -5,15 +5,17 @@
       dev.enable = true;
       info.enable = true;
     };
-    nix.package = pkgs.nixVersions.latest;
-    nix.settings = {
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
+    nix = {
+      package = pkgs.nixVersions.latest;
+      settings = {
+        substituters = [ "https://hyprland.cachix.org" ];
+        trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+      };
+      nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
     };
-    nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
   };
 }
