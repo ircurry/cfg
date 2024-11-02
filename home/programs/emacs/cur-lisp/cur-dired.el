ad8fc55064c1388f1ea2568bb5aafb0a2def4d0e
Ian Curran <icurran@protonmail.com>
2024-11-02 12:10:32 -0400

    emacs: dired keybindings overhaul

---
+++ home/programs/emacs/cur-lisp/cur-dired.el
@@ -0,0 +1,39 @@
+;;; cur-dired.el --- My extensions to dired -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(defun cur-dired-maybe-insert-subdir-or-find-file (&optional other-window)
+  (interactive "P")
+  (let ((file (dired-get-file-for-visit)))
+    (if (file-directory-p file)
+	(if other-window
+	    (dired-other-window file)
+	  (dired-maybe-insert-subdir file nil t))
+      (if other-window
+	  (find-file-other-window file)
+	(dired--find-possibly-alternative-file file)))))
+
+(defun cur-dired-find-file-dwim (&optional other-window)
+  (interactive "P")
+  (let* ((file (dired-get-file-for-visit)))
+    (if (file-directory-p file)
+	(or (when other-window
+	      (progn
+		(dired-other-window file)	
+		(dired-goto-file file)))
+	    (when (and (cdr dired-subdir-alist)
+		       (dired-goto-subdir file))
+	      (when dired-trivial-filenames
+		(dired-goto-next-nontrivial-file))
+	      (run-hooks 'dired-initial-position-hook)
+	      t)
+	    (dired--find-possibly-alternative-file file))
+      (progn
+	(if other-window
+	    (dired-find-file-other-window file)
+	  (dired--find-possibly-alternative-file file))))))
+
+(provide 'cur-dired)
+;;; cur-dired.el ends here
