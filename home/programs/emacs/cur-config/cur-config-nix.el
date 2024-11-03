d8682e16f84242a6abd9bdaf7012a17667488e12
Ian Curran <icurran@protonmail.com>
2024-11-02 23:24:06 -0400

    emacs: fix setting nixd's binary path

--- home/programs/emacs/cur-config/cur-config-nix.el
+++ home/programs/emacs/cur-config/cur-config-nix.el
@@ -7,6 +7,6 @@
   :after lsp-mode
   :custom
   (lsp-disabled-clients '((nix-mode . nix-nil)) "disable nil so that nixd will be used as lsp-server")
-  (setq lsp-nix-nixd-server-path "nixd" "set nixd binary path to be use from current $PATH"))
+  (lsp-nix-nixd-server-path "nixd" "set nixd binary path to be use from current $PATH"))
 
 (provide 'cur-config-nix)
