6ec61df4fe43d5f67720d9e6c464425926469d15
Ian Curran <icurran@protonmail.com>
2025-01-07 11:42:40 -0500

    nixos: remove redundant parentheses

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,9 +1,9 @@
 { host, lib, ... }:
 {
   config = lib.mkMerge [
-    ({
+    {
       networking.hostName = "${host}"; # Define your hostname.
       networking.networkmanager.enable = true;
-    })
+    }
   ];
 }
