776be624a38f3eea3b6605db286e13d4f80c0843
Ian Curran <icurran@protonmail.com>
2025-02-13 18:02:50 -0500

    emacs: add best and worst resolution options
    I also added an option to always prompt for the resolution

--- home/programs/emacs/cur-lisp/cur-yt.el
+++ home/programs/emacs/cur-lisp/cur-yt.el
@@ -10,32 +10,7 @@
   "UI for working with YouTube videos in Emacs."
   :group 'external)
 
-(defcustom cur-yt-default-resolution "1080"
-  "The target resolution for the video.
-If this video resolution is not available the next best resolution will be
-downloaded instead.  It should be one of the common resolution heights YouTube
-uses."
-  :type '(choice (const "2160")
-		 (const "1440")
-		 (const "1080")
-		 (const "720")
-		 (const "480")
-		 (const "360")
-		 (const "240")
-		 (const "144"))
-  :group 'cur-yt)
-
-(defun cur-yt--check-exetutables ()
-  "Check if `mpv' and `yt-dlp'/`youtube-dl' are installed."
-  (unless (executable-find "mpv")
-    (error "`mpv' is not installed on the system, please install it"))
-  (unless (or (executable-find "yt-dlp")
-	      (executable-find "youtube-dl"))
-    (error "Neither `yt-dlp' nor `youtube-dl' are installed, please install one of them"))
-  t)
-
-(defvar cur-yt--mpv-path nil
-  "The executable path to `mpv'.")
+;;; Helper Functions
 
 (defvar cur-yt--youtube-view-key-regexp
   (rx bol
@@ -48,8 +23,9 @@ uses."
 
 (defun cur-yt--get-view-key (url)
   "Get view key from URL."
-  (string-match cur-yt--youtube-view-key-regexp url)
-  (match-string-no-properties 1 url))
+  (save-match-data
+    (string-match cur-yt--youtube-view-key-regexp url)
+    (match-string-no-properties 1 url)))
 
 (defun cur-yt--convert-to-yt-link (url)
   "Convert URL to a YouTube link."
@@ -57,35 +33,74 @@ uses."
     (when key
 	(format "https://www.youtube.com/watch?v=%s" key))))
 
-(defun cur-yt--ytdl-format (height)
+(defun cur-yt--mpv-ytdl-format (height)
   "Return the `mpv' option to get the best video of HEIGHT."
-  (format "--ytdl-format=bestvideo[height<=?%s]+bestaudio/best" height))
+  (pcase height
+      ("best" "--ytdl-format=bestvideo+bestaudio/best")
+      ("worst" "--ytdl-format=worstvideo+worstaudio/worst")
+      ((guard (string-match-p (rx (+ (any "0-9"))) height))
+       (format "--ytdl-format=bestvideo[height<=?%s]+bestaudio/best" height))
+      (_ (error "Cannot format option for mpv, '%s' is not a number" height))))
 
-(defvar cur-yt-url-history nil
-  "Previous YouTube URLs played.")
+;;; Play Video
+
+(defcustom cur-yt-play-default-resolution "1080"
+  "The default video resolution for `cur-yt-play-video'.
+If this video resolution is not available the next best resolution will be
+downloaded instead.  It should be one of the common resolution heights YouTube
+uses, \"best\" or \"worst\"."
+  :type '(choice (const "best")
+		 (const "2160")
+		 (const "1440")
+		 (const "1080")
+		 (const "720")
+		 (const "480")
+		 (const "360")
+		 (const "240")
+		 (const "144")
+		 (const "worst"))
+  :group 'cur-yt)
+
+(defvar cur-yt-play-url-history nil
+  "Previous YouTube URLs played with `cur-yt-play-video'.")
+
+(defun cur-yt--play-check-exetutables ()
+  "Check if `mpv' and `yt-dlp'/`youtube-dl' are installed.
+This function with error if it finds a missing program."
+  (unless (executable-find "mpv")
+    (error "`mpv' is not installed on the system, please install it"))
+  (unless (or (executable-find "yt-dlp")
+	      (executable-find "youtube-dl"))
+    (error "Neither `yt-dlp' nor `youtube-dl' are installed, please install one of them"))
+  t)
+
+(defcustom cur-yt-play-always-prompt-resolution nil
+  "Whether or not `cur-yt-play-video' should always prompt for resolution."
+  :type '(boolean)
+  :group 'cur-yt)
 
 ;;;###autoload
 (defun cur-yt-play-video (url &optional resolution)
   "Play video URL at RESOLUTION."
   (interactive
    (list (read-string "YouTube URL: " nil
-		      cur-yt-url-history
+		      cur-yt-play-url-history
 		      (when kill-ring
 			(cur-yt--convert-to-yt-link (substring-no-properties (car kill-ring)))))
-	 (when current-prefix-arg
+	 (when (or cur-yt-play-always-prompt-resolution current-prefix-arg)
 	   (completing-read "Resolution: "
-			    '("144" "240" "360" "480" "720" "1080" "1440" "2160")
+			    '("worst" "144" "240" "360" "480" "720" "1080" "1440" "2160" "best")
 			    nil 'confirm nil t))))
-  (cur-yt--check-exetutables)
-  (when (and resolution (not (string-match-p (rx (+ (any "0-9"))) resolution)))
-    (user-error "Resolution must be a number"))
-  (let ((res (or resolution cur-yt-default-resolution))
+  (cur-yt--play-check-exetutables)
+  (let ((res (or resolution cur-yt-play-default-resolution))
 	(yt-url (cur-yt--convert-to-yt-link url)))
     (unless yt-url
       (user-error "URL did not contain a YouTube view key"))
     (start-process "my-process" (get-buffer-create "sugma")
-		   "mpv" (cur-yt--ytdl-format res) yt-url)
-    (message "Playing %s at %sp" yt-url res)))
+		   "mpv" (cur-yt--mpv-ytdl-format res) yt-url)
+    (message "Playing %s at %s" yt-url (pcase res
+					 ((or "best" "worst") (concat res " resolution"))
+					 (_ (concat res "p"))))))
 
 (provide 'cur-yt)
 ;;; cur-yt.el ends here
