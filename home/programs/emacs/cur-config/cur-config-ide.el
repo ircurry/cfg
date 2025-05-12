5e30cad0eb2ecb3457a8eae3450b34cbd0c5a51c
Ian Curran <icurran@protonmail.com>
2025-05-12 05:24:25 -0400

    emacs: defer loading nix-mode

--- home/programs/emacs/cur-config/cur-config-ide.el
+++ home/programs/emacs/cur-config/cur-config-ide.el
@@ -138,12 +138,14 @@
   :commands (yaml-mode))
 
 ;; ===nix-mode===
-(use-package nix-mode
+(use-package lsp-mode
+  :defer t
   :hook
   (nix-mode . lsp-deferred)) ;; So that envrc mode will work
 
 (use-package nix-mode
   :after lsp-mode
+  :defer t
   :custom
   (lsp-disabled-clients '((nix-mode . nix-nil)) "disable nil so that nixd will be used as lsp-server")
   (lsp-nix-nixd-server-path "nixd" "set nixd binary path to be use from current $PATH"))
