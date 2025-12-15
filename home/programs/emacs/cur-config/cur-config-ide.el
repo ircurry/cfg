dddbe685c298e7dd1e3390fbc0e834f332699b01
Ian Curran <icurran@protonmail.com>
2025-12-15 15:51:42 -0500

    emacs: remove unused programming language settings and lsp-mode
    I basically do not use any of these programming languages anymore and
    I probably won't for the foreseeable future. If I do I will want to do
    it with built in packages rather than 3rd party packages.

--- home/programs/emacs/cur-config/cur-config-ide.el
+++ home/programs/emacs/cur-config/cur-config-ide.el
@@ -8,51 +8,6 @@
   :demand t
   :hook (prog-mode . display-fill-column-indicator-mode))
 
-;; ===LSP Mode===
-(use-package lsp-mode
-  :disabled t
-  :demand t
-  :hook
-  (lsp-mode  . lsp-enable-which-key-integration)
-  :custom
-  (lsp-keymap-prefix "C-c C-M-l" "lsp mode keymap")
-  (lsp-file-watch-threshold 1750)
-  (lsp-headerline-breadcrumb-enable nil)
-  :bind (:map lsp-mode-map
-              ("C-c C-a" . lsp-execute-code-action)  ; code actions
-              ("C-c C-e" . lsp-treemacs-errors-list) ; treemacs error list
-              ("C-c f"   . lsp-find-references)      ; find references
-              ("C-c r"   . lsp-find-definition))     ; find definitions
-  :config
-  (lsp-deferred)
-  (setq gc-cons-threshold (* 100 1024 1024))
-  (setq read-process-output-max (* 3 1024 1024))
-  (setq lsp-idle-delay 0.500)
-  (setq lsp-lens-enable nil)
-  (lsp-enable-which-key-integration t))
-
-(use-package lsp-ui
-  :disabled t
-  :after (lsp-mode)
-  :custom
-  (lsp-ui-doc-enable nil "lsp-ui doc disabled by default")
-  (lsp-ui-doc-show-with-cursor t "lsp-ui doc follows cursor")
-  (lsp-ui-doc-show-with-mouse t "lsp-ui doc follows mouse")
-  (lsp-ui-doc-position 'at-point "lsp-ui doc shows at cursor")
-  :bind (:map lsp-ui-mode-map
-              ("C-c C-f" . lsp-ui-peek-find-references)  ; find references ui
-              ("C-c C-r" . lsp-ui-peek-find-definitions) ; find definitions ui
-              ("C-c C-d" . lsp-ui-doc-mode) ; toggle doc mode
-              :map lsp-ui-peek-mode-map
-              ("ESC" . lsp-ui-peek--abort)             ; toggle doc mode
-              ("g"   . lsp-ui-peek--abort)             ; toggle doc mode
-              ("j"   . lsp-ui-peek--select-next)       ; toggle doc mode
-              ("k"   . lsp-ui-peek--select-prev)       ; toggle doc mode
-              ("C-j" . lsp-ui-peek--select-next-file)  ; toggle doc mode
-              ("C-k" . lsp-ui-peek--select-prev-file)) ; toggle doc mode
-  :hook
-  (lsp-mode . lsp-ui-mode))
-
 (use-package eglot
   :ensure nil
   :bind ( :map eglot-mode-map
@@ -147,28 +102,6 @@
   :after (java-ts-mode)
   :hook (java-ts-mode . eglot-ensure))
 
-;; ===lsp-java===
-(use-package lsp-java
-  :disabled t
-  :after (java-ts-mode lsp-mode cc-mode envrc)
-  :hook
-  (envrc-mode . (lambda ()
-                  (when (equal major-mode 'java-ts-mode)
-                    (setq lsp-java-server-install-dir (concat (getenv "JDTLS_PATH") "/share/java/jdtls/")))))
-  (java-ts-mode . lsp-deferred)
-  :config
-  (defun lsp-java--ls-command ()
-    (let ((jdtls-path (getenv "JDTLS_PATH"))
-          (jdtls-exec-options (list
-                               "-configuration"
-                               (concat (getenv "HOME") "/.jdtls/config_linux")
-                               "-data"
-                               (concat (getenv "HOME") "/.jdtls/java-workspace"))))
-      (message (concat jdtls-path "/share/java/"))
-      (append (list (concat jdtls-path "/bin/jdtls")) jdtls-exec-options))))
-
-(use-package just-mode)
-
 ;; ===YAML===
 (use-package yaml-mode
   :commands (yaml-mode))
@@ -189,69 +122,6 @@
   :custom
   (lsp-disabled-clients '((nix-mode . nix-nil)) "disable nil so that nixd will be used as lsp-server")
   (lsp-nix-nixd-server-path "nixd" "set nixd binary path to be use from current $PATH"))
-
-;; ===Rust-Mode===
-(use-package rustic
-  :custom (rustic-lsp-client 'eglot))
-
-(use-package elec-pair
-  :after rustic
-  :demand t
-  :hook (rustic-mode . electric-pair-local-mode))
-
-(use-package eglot
-  :ensure nil
-  :after (rustic)
-  :hook (rustic-mode . eglot-ensure))
-
-(use-package lsp-mode
-  :disabled t
-  :after (rustic)
-  :hook (rustic-mode . lsp-deferred))
-
-;; ===Haskell-Mode===
-(use-package haskell-mode)
-
-(use-package eglot
-  :ensure nil
-  :after (haskell-mode)
-  :hook (haskell-mode . eglot-ensure))
-
-(use-package lsp-mode
-  :disabled t
-  :after (haskell-mode)
-  :hook (haskell-mode . lsp-deferred))
-
-;; ===LSP-Haskell===
-(use-package lsp-haskell
-  :disabled t
-  :after (haskell-mode lsp-mode)
-  :demand t)
-
-;; ===Company-GHCI===
-(use-package company-ghci
-  :after (company haskell-mode)
-  :demand t
-  :custom (company-ghc-show-info t)
-  :config
-  (push 'company-ghci company-backends))
-
-;; ===Tuareg===
-(use-package tuareg)
-
-;; ===Utop===
-(use-package utop
-  :commands (utop utop-mode)
-  :config
-  (advice-add 'utop :around 'inheritenv-apply))
-
-(use-package merlin
-  :after (tuareg)
-  :hook (tuareg-mode . merlin-mode))
-
-(use-package merlin-company
-  :after (company merlin)
-  :demand t)
 
 ;; ===C Tree-Sitter Mode===
 (use-package c-ts-mode
@@ -281,19 +151,6 @@
   :disabled t
   :after (cc-mode c-ts-mode lsp-mode)
   :demand t)
-
-;; ===Zig Mode===
-(use-package zig-mode)
-
-(use-package eglot
-  :ensure nil
-  :after (zig-mode)
-  :hook (zig-mode . eglot-ensure))
-
-(use-package lsp-mode
-  :disabled t
-  :after (zig-mode)
-  :hook (zig-mode . lsp-deferred))
 
 ;; ===Go Tree-Sitter Mode===
 (use-package go-ts-mode
